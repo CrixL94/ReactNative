750dedc641cc083d953705c9b1ba89b191f48668
CrisLopez94 <clopez@ppf.com.hn>
2024-12-06 13:16:12 -0600

    funciones e intalacion de tailwindcss

--- bases/src/App.tsx
+++ bases/src/App.tsx
@@ -1,15 +1,16 @@
-import "./App.css";
+// import { Funciones } from "./typescript/Funciones";
 // import { ObjectLiteral } from "./typescript/ObjectLiteral";
 // import { BasicTypes } from "./typescript/Basictypes";
 
 function App() {
   return (
-    <>
-      <h1>React + TS</h1>
+    <div className="flex justify-center items-center h-svh">
+      <h1 className="text-3xl">React + TS</h1>
       {/* <BasicTypes/> */}
       {/* <ObjectLiteral/> */}
+      {/* <Funciones/> */}
 
-    </>
+    </div>
   );
 }
 
---
+++ bases/src/typescript/Funciones.tsx
@@ -0,0 +1,13 @@
+const addTwoNumbers = (a: number, b: number): string => {
+  return `${a + b}`;
+};
+
+export const Funciones = () => {
+
+  return (
+    <>
+    <h3>Funciones</h3>
+    <span>El resultado de suma 2+8 = {addTwoNumbers(2,8)}</span>
+    </>
+  )
+};
